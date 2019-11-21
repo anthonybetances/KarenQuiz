d2d673b855affcfbb30cb5a28db160f2ba595d74
Betances <tonybetancesdev@gmail.com>
2019-11-21 07:50:41 -0500

    First commit. Not completed.

---
+++ public/main.js
@@ -0,0 +1,145 @@
+
+
+
+
+
+
+
+// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+// var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+// var trash = document.getElementsByClassName("fa-trash");
+//
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+//
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messagesAlt', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+//
+//
+// Array.from(trash).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         fetch('messages', {
+//           method: 'delete',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg
+//           })
+//         }).then(function (response) {
+//           window.location.reload()
+//         })
+//       });
+// });
+//
+//
+document.querySelector("form").addEventListener("submit", nameGen)
+
+function nameGen (e){
+//
+//   fetch('inventoryUpdate', {
+//             method: 'put',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//               'title': title,
+//               'author': author,
+//               'available':available
+//             })
+//           })
+//           .then(response => {
+//             if (response.ok) return response.json()
+//           })
+//           .then(data => {
+//             console.log(data)
+//             window.location.reload(true)
+//           })
+
+
+  e.preventDefault()
+  let nameTech = document.querySelectorAll(".tech:checked")[0].value;
+  let namePlace = document.querySelectorAll(".place:checked")[0].value;
+  let nameCall = document.querySelectorAll(".call:checked")[0].value;
+  let nameOutfit = document.querySelectorAll(".outfit:checked")[0].value;
+  let nameCity = document.querySelectorAll(".city:checked")[0].value;
+
+  class Choices {
+    constructor(tech, place, call, outfit, city){
+      this.tech = parseInt(tech),
+      this.place = parseInt(place),
+      this.call = parseInt(call),
+      this.outfit = parseInt(outfit),
+      this.city = parseInt(city)
+    }
+  }
+  let newName = new Choices(nameTech, namePlace, nameCall, nameOutfit, nameCity);
+
+  let total = 0;
+  Object.values(newName).forEach(num => {
+    total = total+num;
+  })
+  console.log(checkResult(total))
+  document.querySelector("span").textContent = checkResult(total);
+}
+
+  function checkResult(total){
+    if (total === 5){
+      return "BBQ Becky"
+    }else if(total === 6){
+      return "Callbox Christopher"
+    }else if(total === 7){
+      return "Apartment Patty"
+    }else if(total === 8){
+      return "Cornerstore Caroline"
+    }else if(total === 9){
+      return "Baggage Claim Becky"
+    }else if(total === 10){
+      return "AppleCare Karen"
+    }else{
+      return "Try Again"
+    }
+  }
